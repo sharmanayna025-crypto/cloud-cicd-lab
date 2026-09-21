ddfb5cd5e19ff38471fdaf822fa25b969185b215
Nayna Sharma <sharmanayna025@gmail.com>
2026-09-21 09:42:00 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Cloud CI/CD Lab - Version 1"
+
+@app.route("/student")
+def student():
+    return {
+        "name": "Student",
+        "course": "Cloud Computing and DevOps",
+        "version": "1.0"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
